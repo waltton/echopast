b9b301a43feb11be1987063209d676bdd06265f4
Waltton Morais <walttonm@gmail.com>
2022-09-24 16:06:22 +0400

    WIP

--- whois/whois.go
+++ whois/whois.go
@@ -1,12 +1,28 @@
 package whois
 
 import (
-	"fmt"
+	"bufio"
 	"io"
 	"log"
 	"net"
+	"regexp"
+	"strings"
 	"time"
 )
+
+type Whois struct {
+	Comments []string `json:"comments"`
+
+	Refer        string `json:"refer"`
+	InetNum      string `json:"inetnum"`
+	InetNumBegin net.IP `json:"inetnum_begin"`
+	InetNumEnd   net.IP `json:"inetnum_end"`
+	Organisation string `json:"organisation"`
+	Status       string `json:"status"`
+	Whois        string `json:"whois"`
+	Changed      string `json:"changed"`
+	Source       string `json:"source"`
+}
 
 func Lookup(param string) (result string, err error) {
 	addr := "whois.iana.org:43"
@@ -14,6 +30,13 @@ func Lookup(param string) (result string, err error) {
 	if err != nil {
 		return "", err
 	}
+
+	whois, err := parseWhois(result)
+	if err != nil {
+		return "", err
+	}
+
+	_ = whois
 
 	return
 }
@@ -24,18 +47,15 @@ func rawQuery(addr, param string) (result string, err error) {
 		return "", err
 	}
 
-	n, err := conn.Write([]byte(param + "\r\n"))
+	_, err = conn.Write([]byte(param + "\r\n"))
 	if err != nil {
 		return
 	}
 
-	fmt.Println("n", n)
-
 	for {
 		conn.SetReadDeadline(time.Now().Add(time.Second * 30))
 
-		data := make([]byte, 10)
-		// data := make([]byte, 4096)
+		data := make([]byte, 4096)
 		_, err := conn.Read(data)
 		if err != nil {
 			if err == io.EOF || err.(net.Error).Timeout() {
@@ -48,6 +68,58 @@ func rawQuery(addr, param string) (result string, err error) {
 
 		result += string(data)
 	}
+
+	return
+}
+
+var reKV = regexp.MustCompile(`(\w*):\s*(.*)`)
+
+func parseWhois(raw string) (w *Whois, err error) {
+	w = new(Whois)
+
+	var i int
+	scanner := bufio.NewScanner(strings.NewReader(raw))
+	for scanner.Scan() {
+		i++
+
+		if scanner.Text() == "" {
+			continue
+		}
+
+		if strings.HasPrefix(scanner.Text(), "%") {
+			w.Comments = append(w.Comments, scanner.Text())
+			continue
+		}
+
+		m := reKV.FindStringSubmatch(scanner.Text())
+		if len(m) == 3 {
+			switch m[1] {
+			case "refer":
+				w.Refer = m[2]
+			case "inetnum":
+				w.InetNum = m[2]
+				ins := strings.Split(w.InetNum, " - ")
+				w.InetNumBegin = net.ParseIP(ins[0])
+				w.InetNumEnd = net.ParseIP(ins[1])
+			case "organisation":
+				w.Organisation = m[2]
+			case "status":
+				w.Status = m[2]
+			case "whois":
+				w.Whois = m[2]
+			case "changed":
+				w.Changed = m[2]
+			case "source":
+				w.Source = m[2]
+			}
+
+		} else {
+			log.Printf("fail to parse line:'%s'", scanner.Text())
+		}
+
+	}
+
+	err = scanner.Err()
 
 	return
 }
--- whois/whois_test.go
+++ whois/whois_test.go
@@ -1,9 +1,12 @@
 package whois
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -12,4 +15,30 @@ func TestLookup(t *testing.T) {
 	require.NoError(t, err)
 
 	fmt.Println("result", result)
+}
+
+func TestParse(t *testing.T) {
+
+	testCases := []struct {
+		name string
+	}{
+		{name: "1.1.1.1-iana"},
+	}
+
+	for _, tc := range testCases {
+		raw, err := os.ReadFile("./data/" + tc.name + ".txt")
+		require.NoError(t, err)
+
+		f, err := os.Open("./data/" + tc.name + ".json")
+		require.NoError(t, err)
+
+		ew := &Whois{}
+		err = json.NewDecoder(f).Decode(&ew)
+		require.NoError(t, err)
+
+		result, err := parseWhois(string(raw))
+		require.NoError(t, err)
+
+		assert.Equal(t, ew, result)
+	}
 }
